2625fc5916afcbf216de8a7308649d62e7308760
Skye Kaijser <skye@kth.se>
2024-10-07 08:47:02 +0200

    Added useable string buffers so I can work with this

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 typedef enum {
     IDENTIFIER, // var identifier
     KEYWORD,    // keyword
@@ -47,6 +49,36 @@ char* keywords[]={
 };
 
 
+typedef struct {
+	char* ptr;
+	size_t len;
+} StrBuf;
+int strLen(char* str){
+	int ln=0;
+	while(1){
+		if(str[ln]=='\0'){
+			return ln-1;
+		}
+		ln++;
+	}
+}
+
+void initStrBuf(StrBuf* str){
+	str->len=0;
+	str->ptr=malloc(1);
+	if (str->ptr == NULL){
+		printf("\033[31mCouldn't init pointer for str\033[0m\n");
+		exit(5);
+	}	
+}
+
+void writeChar(StrBuf* str, char chr){
+	str->len+=1;
+	str->ptr=realloc(str->ptr,str->len+1);
+	str->ptr[str->len-1]=chr;
+	str->ptr[str->len]='\0';
+}
+
 typedef enum {
     Integer,
     Float,
@@ -60,8 +92,17 @@ typedef struct {
 } Value;
 
 
-
+typedef struct {
+	char* expr;
+	int index;
+	char* current;
+} Lexer;
 int main() {
-printf("Hellooo");
-printf("%d",LCURL);
+printf("Hellooo\n");
+StrBuf* str;
+initStrBuf(str);
+writeChar(str,'a');
+printf("%d\n%s\n",LCURL,str->ptr);
+writeChar(str,'a');
+printf("%s\n",str->ptr);
 }
